c3d3ec65397d74540600f94edbdb2418333ab8fb
Josh <toyjtoy@gmail.com>
2015-10-22 19:30:56 -0400

    scoreboards

--- js/rpslogic.js
+++ js/rpslogic.js
@@ -1,10 +1,32 @@
+////////////////////////////////////////////////
+/*   Provided Code - Please Don't Edit   */
+////////////////////////////////////////////////
 'use strict';
-var move;
 
-// function getInput() {
-//     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-//     return prompt();
-// }
+
+
+$( document ).ready(function() {
+
+  $( "button" ).click(function() {
+    $( ".col-xs-12" ).hide( "slow" );
+    $( "#gameon" ).show( "slow" );
+    playToFive();
+  });
+
+
+  $( ".pic img" ).click(function() {
+  move = $(this).attr('id');
+  });
+
+
+
+});
+
+
+function getInput() {
+    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    return prompt();
+}
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -15,23 +37,27 @@ function randomPlay() {
         return "scissors";
     }
 }
+////////////////////////////////////////////////
+/*           Write Your Code Below            */
+////////////////////////////////////////////////
 
+    // Write an expression that operates on a variable called `move`
+    // If a `move` has a value, your expression should evaluate to that value.
+    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
 function getPlayerMove(move) {
-    var move;
-    move = move;
+    var move
+        if (move != null){
+            move = move;
+        }
+        else {
+            move = getInput();
+        }
+    return move
 }
 
-// function getPlayerMove(move) {
-//     var move
-//         if (move != null){
-//             move = move;
-//         }
-//         else {
-//             move = getInput();
-//         }
-//     return move
-// }
-
+    // Write an expression that operates on a variable called `move`
+    // If a `move` has a value, your expression should evaluate to that value.
+    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
 function getComputerMove(move) {
     var move
         if (move != null){
@@ -44,6 +70,10 @@ function getComputerMove(move) {
     return move
 }
 
+    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
+    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+    /* YOUR CODE HERE */
 function getWinner(playerMove,computerMove) {
     var winner;
     if(playerMove == "rock" && computerMove == "paper"){
@@ -77,6 +107,8 @@ function getWinner(playerMove,computerMove) {
     return winner;
 }
 
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    /* YOUR CODE HERE */
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
@@ -86,14 +118,12 @@ function playToFive() {
 
         if (winner == "player") {
         playerWins++;
-        console.log([playerWins, computerWins]);
-        }
-        else if (winner == "computer") {
-        computerWins++;
-        console.log([playerWins, computerWins]);
-        }
-        else {
-        console.log("No change in score.");
+            $('.humanwins').html(parseInt($('.humanwins').html(), 10)+1);
+        } else if (winner == "computer") {
+            computerWins++;
+            $('.computerwins').html(parseInt($('.computerwins').html(), 10)+1);
+        } else {
+            $('.ties').html(parseInt($('.ties').html(), 10)+1);
         }
 
    }
--- js/ui.js
+++ js/ui.js
@@ -12,4 +12,4 @@ $( document ).ready(function() {
 
 
 
-}); //End of doc ready function
+});
